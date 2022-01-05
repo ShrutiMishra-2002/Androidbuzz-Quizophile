3058832dd1905e16297c6e5357e080c003128bab
Shruti Mishra <shrutidmishra2002@gmail.com>
2022-01-05 22:57:41 +0530

    fixed splash screen

--- app/src/main/java/com/example/quizapp/splashActivity2.kt
+++ app/src/main/java/com/example/quizapp/splashActivity2.kt
@@ -3,18 +3,19 @@ package com.example.quizapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 
-class splashActivity2 : AppCompatActivity() {
+class SplashActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash2)
-
-        val button = findViewById<Button>(R.id.Letstart)
-        button.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }
+        }, 6000)
     }
 }
+
+
