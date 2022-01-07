c06172a130b2ec2696b09bd2c831eb626d7c2d7c
Niket <52085669+nikeight@users.noreply.github.com>
2022-01-07 21:25:30 +0530

    Nav Drawer, Action_View Intents added (#2)
    * added: NavDrawerLayout and MaterialApp bar
    
    * added: github,linkedin links
    
    Co-authored-by: Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -9,9 +9,11 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
         button.setOnClickListener {
             if(input.text.toString().isEmpty())
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             }
             else{
                 var intent = Intent(this,QuestionActivity::class.java)
-                intent.putExtra("${setData.name}",input.text.toString())
+                intent.putExtra(QuestionDummyData.name,input.text.toString())
                 startActivity(intent)
                 finish()
             }
